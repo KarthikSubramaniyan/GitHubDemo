f404fc3045de157fccaf5e5819496f359562edf9
kalaivani <kalaivani@localhost>
2020-07-06 13:13:00 +0530

    Code Update

--- src/Test1.java
+++ src/Test1.java
@@ -1,11 +1,34 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		System.out.println("Github");
+	public static void main(String[] args) throws InterruptedException {
 
+		System.setProperty("webdriver.chrome.driver", "/Users/kalaivani/Downloads/Karthik/Project/chromedriver");
+
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+//		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+						
+		driver.get("https://login.salesforce.com/");
+				
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("karthik.subramaniyan-bf3b@force.com");
+		
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Keyboard@98765");
+		
+//		driver.findElement(By.xpath("//input[@id='Login']")).click();
+//		
+//		Thread.sleep(800000);
+//				
+//		System.out.println(driver.switchTo().alert().getText());	
+				
 	}
 
 }
